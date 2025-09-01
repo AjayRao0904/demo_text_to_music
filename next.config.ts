3891718e668358555d492340a08df2f8c72c625d
ajayrao <ajayrao0904@gmail.com>
2025-09-01 17:25:10 +0530

    eh

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "/songgenerator",
-  assetPrefix: "/songgenerator", // optional, ensures static assets also resolve correctly
+  
 };
 
 module.exports = nextConfig;
