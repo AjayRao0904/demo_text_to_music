eb536ec0eebe96feae52e5e1168351e2ca07bf85
ajayrao <ajayrao0904@gmail.com>
2025-09-01 17:07:47 +0530

    idl

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: "/songgenerator",
+  assetPrefix: "/songgenerator", // optional, ensures static assets also resolve correctly
 };
 
 module.exports = nextConfig;
